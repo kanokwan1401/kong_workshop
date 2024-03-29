b718cb003126ad2194a84816db7b02ff45c364e7
wiKanok <kanokwan1401.ks@gmail.com>
2024-03-29 13:25:14 +0700

    w-10 finish

--- w10_searchfilter/src/App.js
+++ w10_searchfilter/src/App.js
@@ -1,11 +1,83 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
+  const [countries, setCountries] = useState([]);
+  const [word, setWord] = useState("");
+  const [dataFilter] = useState(["name", "capital"]);
 
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/all") //update link api
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data);
+      });
+  }, []);
+
+  const formatNumber = (num) => {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  };
+
+  const searchCountries = (countries) => {
+    return countries.filter((item) => {
+      // eslint-disable-next-line
+      return dataFilter.some((filter) => {
+        if (item[filter]) {
+          return (
+            item[filter].toString().toLowerCase().indexOf(word.toLowerCase()) >
+            -1
+          );
+        }
+      });
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="search-container">
+        <label htmlFor="search-form">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="search"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
+        </label>
+      </div>
+      <ul className="row">
+        {searchCountries(countries).map((item, index) => {
+          return (
+            <li key={index}>
+              <div className="card">
+                <div className="card-title">
+                  <img src={item.flag} alt={item.name} />
+                </div>
+                <div className="card-body">
+                  <div className="card-dascription">
+                    <h2>{item.name}</h2>
+                    <ol className="card-list">
+                      <li>
+                        Population :{" "}
+                        <span>{formatNumber(item.population)}</span>
+                      </li>
+                      <li>
+                        Region : <span>{item.region}</span>
+                      </li>
+                      <li>
+                        Capital : <span>{item.capital}</span>
+                      </li>
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
